a423596eb2d5c864055395f3810bb677de486050
ben <x_z_gfree@163.com>
2022-05-01 21:24:27 +0800

    修改 处理路由跳转

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,21 @@
 import { DashboardEntryPath, LoginPath } from "@src/constant";
 import userModel from "@src/model/user";
 import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 /**
  * 根据用户 token 状态跳转
  * @returns
  */
 export default function Home() {
+  const history = useHistory();
   useEffect(() => {
     if (userModel.isLogin) {
-      window.location.href = `/#${DashboardEntryPath}`;
+      history.push(DashboardEntryPath);
     } else {
       // TODO: 这里应该是扫码登陆页面
-      window.location.href = `/#${LoginPath}`;
+      history.push(LoginPath);
     }
-  }, []);
+  }, [history]);
   return <div />;
 }
