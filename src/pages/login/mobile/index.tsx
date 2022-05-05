270468fff45678c26d0fec4ea0342414aefe6c29
ben <x_z_gfree@163.com>
2022-05-05 15:04:57 +0800

    修改 添加 mobile 跳转提示

--- src/pages/login/mobile/index.tsx
+++ src/pages/login/mobile/index.tsx
@@ -1,9 +1,27 @@
+import { colors } from "@src/constant";
 import SignUp from "@src/pages/login/components/sign_up";
-import React, { memo } from "react";
+import { rgba } from "@src/utils/css";
+import { Modal } from "antd";
+import React, { memo, useCallback } from "react";
 import { BaseProps } from "../types";
 import "./index-mobile.scoped.scss";
 
 const MobileView = memo(({ onSignUp }: BaseProps) => {
+  const handleSignUp = useCallback(() => {
+    Modal.info({
+      title: "请使用 PC 访问",
+      content: "移动端正在开发中，请在 PC 上访问",
+      centered: true,
+      width: 316,
+      bodyStyle: {
+        borderRadius: "16px",
+      },
+      maskStyle: {
+        background: rgba(colors["neutral-02"], 0.8),
+      },
+    });
+  }, []);
+
   return (
     <div className="flex-col flex h-100vh">
       <div className="sign-in-box flex justify-between">
@@ -14,7 +32,7 @@ const MobileView = memo(({ onSignUp }: BaseProps) => {
         </div>
       </div>
       <div className="flex-1 flex justify-center items-center">
-        <SignUp onSignUp={onSignUp} />
+        <SignUp onSignUp={handleSignUp} />
       </div>
     </div>
   );
